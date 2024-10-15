66a2dcaf9b74020c5cd3f06dc75d574c038da66b
Ibrahim Amin <58993233+notEpsilon@users.noreply.github.com>
2024-10-15 17:40:24 +0300

    Update layout.tsx

--- client/src/components/domain/layout.tsx
+++ client/src/components/domain/layout.tsx
@@ -3,7 +3,6 @@ import Header from "./header";
 import Container from "./container";
 import { Toaster } from "../ui/sonner";
 import { useSession } from "@/hooks/use-session";
-import { LucideLoader2 } from "lucide-react";
 import { Redirect } from "wouter";
 
 type Props = {
@@ -12,7 +11,7 @@ type Props = {
 };
 
 const Layout: FC<Props> = ({ children, protectedPage = false }) => {
-  const { user, isLoading } = useSession();
+  const { user } = useSession();
 
   return (
     <div className="min-h-screen">
